8216270400ee103116158144c133464caad83197
danielmeeks153 <danielmeeks153@gmail.com>
2021-12-09 13:09:27 -0700

    Done

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Main.js
@@ -0,0 +1,22 @@
+alert(" Hello World"); 
+window.alert(" Hello World!");
+document.write(' Hello, World'); 
+var A = "\" This is a string"; 
+document.write(A); 
+document.write(" Hello World" + " goodbye world") 
+var P = (" Here we are typing stuff" + " and more stuff") 
+document.write(P)
+
+
+var a = " one", b = " two", c = " three"; 
+var a = a.fontcolor("blue"); 
+document.write(a); 
+
+document.write(3+30); 
+
+
+function My_First_Function() {//defining the name of the function 
+    var str = " This text is green!";//text var
+    var result = str.fontcolor("green");//what we are going to print var
+    document.getElementById("Green_Text").innerHTML = result; //calling the var we want to sho
+}
